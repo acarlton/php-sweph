72f2c34a1c0f1be00943e97863ba605f3f25f181
Aaron Carlton <aaron.r.carlton@gmail.com>
2017-05-07 13:43:03 -0500

    fixing swe_sol_eclipse_when_loc for php7

--- sweph.c
+++ sweph.c
@@ -1292,7 +1292,7 @@ PHP_FUNCTION(swe_sol_eclipse_when_loc)
 	char serr[AS_MAXCH]; 
 	int i;
 	int backward;
-	zval *tret_arr, *attr_arr;
+	zval tret_arr, attr_arr;
 
 	if(ZEND_NUM_ARGS() != 6) WRONG_PARAM_COUNT;
 
@@ -1311,18 +1311,15 @@ PHP_FUNCTION(swe_sol_eclipse_when_loc)
 	}
 	else
 	{
-		MAKE_STD_ZVAL(tret_arr);
-		array_init(tret_arr);
+		array_init(&tret_arr);
 		for(i = 0; i < 10; i++)
-			add_index_double(tret_arr, i, attr[i]);
-		add_assoc_zval(return_value, "tret", tret_arr);
+			add_index_double(&tret_arr, i, attr[i]);
+		add_assoc_zval(return_value, "tret", &tret_arr);
 
-		MAKE_STD_ZVAL(attr_arr);
-		array_init(attr_arr);
+		array_init(&attr_arr);
 		for(i = 0; i < 20; i++)
-			add_index_double(attr_arr, i, attr[i]);
-		
-		add_assoc_zval(return_value, "attr", attr_arr);
+			add_index_double(&attr_arr, i, attr[i]);
+		add_assoc_zval(return_value, "attr", &attr_arr);
 	}
 }
 
